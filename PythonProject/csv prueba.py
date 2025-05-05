c5642db4526f8cd02d4b9508a8a98625a949a9ea
greenvick147 <145700524+catviky147@users.noreply.github.com>
2025-05-05 07:59:20 -0500

    actualizacion csv

--- PythonProject/csv prueba.py
+++ PythonProject/csv prueba.py
@@ -1,16 +1,11 @@
-import csv
 def cargar_tablero_desde_csv(nombre_archivo):
     tablero = []
-    with open(nombre_archivo, newline='') as archivo:
-        lector = csv.reader(archivo)
-        for fila in lector:
-            # Convertimos cada valor de texto a entero
-            tablero.append([int(celda) for celda in fila])
+    archivo= open(nombre_archivo, "r")
+     
+    tablero=archivo.read()
     return tablero
 
+#codigo principal
 tablero = cargar_tablero_desde_csv('juego de la vida.csv')
 
-for sublista in tablero:
-    for elemento in sublista:
-        print(elemento, end=" ") 
-    print()
+print (tablero)
